bb37497696e2f270805171bdfa8af426f6ef6ea5
bhyahmed98 <ahmedbhy1@gmail.com>
2021-07-14 10:34:29 +0100

    test commit

---
+++ backend/demo/src/main/java/com/example/demo/model/UserInfo.java
@@ -0,0 +1,41 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+import lombok.Data;
+
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Table(name = "user")
+public class UserInfo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+	@SequenceGenerator(name = "user_seq", sequenceName = "user_seq")
+	private long idUser;
+	private String name;
+	private String email;
+	private String password;
+	private String phonenumber;
+	
+	// this.urlimage = urlimage;
+
+}
