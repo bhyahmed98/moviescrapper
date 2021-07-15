6717af75626fb0c720a7b3fafe44aa25f2d726dd
Sarra Merchaoui <sarra.merchaoui@supcom.tn>
2021-07-15 12:50:16 +0100

    edited

--- backend/demo/src/main/java/com/example/demo/model/Category.java
+++ backend/demo/src/main/java/com/example/demo/model/Category.java
@@ -1,32 +1,15 @@
 package com.example.demo.model;
 
 
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import static javax.persistence.TemporalType.TIMESTAMP;
-
-import java.util.HashSet;
 import java.util.Set;
 
 import lombok.Data;
