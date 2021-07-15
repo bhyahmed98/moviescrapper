e0eb7ca80bb9525fd4c23bb50ddf862c386d7635
Sarra01 <66032653+Sarra01@users.noreply.github.com>
2021-07-15 09:38:51 +0100

    Update Film.java

--- backend/demo/src/main/java/com/example/demo/model/Film.java
+++ backend/demo/src/main/java/com/example/demo/model/Film.java
@@ -28,53 +28,16 @@ import lombok.Data;
 @Table(name = "Film")
 public class Film {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO, generator = "Film_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Film_seq")
 	@SequenceGenerator(name = "Film_seq", sequenceName = "Film_seq")
 	private long idFilm;
 	private String nameFilm;
 	private int idCategory;
 	private int year;
 	private String language;
-	private int numRate;
-	private int curRate;
+	private int numberRate;
+	private int currentRate;
 
 	
-	// public Product(@JsonProperty("id")Long id,
-	// @JsonProperty("description")String description,
-	// @JsonProperty("created_at")Date created_at,
-	// @JsonProperty("price")float price,
-	// @JsonProperty("stock")float stock,
-	// @JsonProperty("brand")String brand) {
-	// super();
-	// this.id = id;
-	// this.description = description;
-	// this.created_at = created_at;
-	// this.price = price;
-	// this.stock = stock;
-	// this.brand = brand;
-	// }
-
-	// @Column(name = "name", nullable = false)
-	// private String name;
-	//
-	// @Column(name = "madeIn", nullable = false)
-	// private String madeIn;
-
-	// public Article(@JsonProperty("idArticle") int idArticle,
-	// @JsonProperty("nameArticle") String nameArticle,
-	// @JsonProperty("couleur") String couleur,
-	// @JsonProperty("taille") int taille,
-	// @JsonProperty("qte") int qte,
-	// @JsonProperty("prix") float prix,
-	// @JsonProperty("prixT") float prixT,
-	// String urlimage) {
-	// this.idArticle = idArticle;
-	// this.nameArticle = nameArticle;
-	// this.couleur = couleur;
-	// this.taille = taille;
-	// this.qte = qte;
-	// this.prix = prix;
-	// this.prixT = prixT;
-	// this.urlimage = urlimage;
 
 }
