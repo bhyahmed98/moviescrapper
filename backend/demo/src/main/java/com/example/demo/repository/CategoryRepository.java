661158ec6cd6c61fef635b5a2e662c28f8455abe
bhyahmed98 <ahmedbhy1@gmail.com>
2021-07-21 21:36:24 +0100

    a

--- backend/demo/src/main/java/com/example/demo/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- backend/demo/src/main/java/com/example/demo/repository/FilmRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Film;
-
-public interface FilmRepository extends JpaRepository<Film, Long> {
-}
